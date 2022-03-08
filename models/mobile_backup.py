406b93606e215bd0dd28f77da98c812f3df84a9c
Zhihao Duan <duan90@purdue.edu>
2022-03-08 17:59:02 -0500

    backup

--- models/mobile_backup.py
+++ models/mobile_backup.py
@@ -7,6 +7,8 @@ import torchvision as tv
 from compressai.entropy_models import EntropyBottleneck, GaussianConditional
 from mycv.utils.coding import compute_bpp
 
+from models.base import MobileCloudBase
+
 
 class IntegerQuantization(nn.Module):
     def __init__(self, n_ch):
@@ -104,43 +106,6 @@ def get_entropy_model(name, channels):
     else:
         raise ValueError()
     return entropy_model
-
-
-class MobileCloudBase(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.testing_stats = (0, 0.0, 0.0) # num, bpp, bits per dim
-        self.detach = False
-        self.flops_mode = False
-
-    def init_testing(self):
-        self.testing_stats = (0, 0.0, 0.0) # num, bpp, bits per dim
-
-    @amp.autocast(enabled=False)
-    def forward_entropy(self, z):
-        if self.detach:
-            z = z.detach().float()
-        else:
-            z = z.float()
-        z, p_z = self.entropy_model(z)
-        return z, p_z
-
-    def forward_cls(self, x):
-        assert not self.training
-        yhat, p_z = self.forward(x)
-
-        # update testing stats
-        num, bpp, bpd = self.testing_stats
-        nB, _, imh, imw = x.shape
-        batch_bpp = -1.0 * torch.log2(p_z).sum() / (imh*imw)
-        # batch_bpp = compute_bpp(p_z, imh*imw, batch_reduction='sum')
-        bpp = (bpp * num + batch_bpp) / (num + nB)
-        batch_bpd = - torch.log2(p_z).mean() * nB
-        bpd = (bpd * num + batch_bpd) / (num + nB)
-        num += nB
-        self.testing_stats = (num, bpp.item(), bpd.item())
-
-        return yhat
 
 
 class ResNet50MC(MobileCloudBase):
